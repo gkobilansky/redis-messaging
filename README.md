e60d58e95820c60aa93a7e50413f63fe8aee3c10
Gene Kobilansky <genek149@gmail.com>
2018-04-18 23:16:07 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# WonderQ
+
+A simple message queue using node, Redis and the Serverless framework
+
+## Features
+
+* Initialize new queue in your Redis server
+* List available queues
+* Send and receive messages
+* Received messages are hidden in queue for 30 seconds (configurable via init)
+* Received messages can be deleted using message ID
+
+**Please note:** although you can initialize multiple queues, current implementation for send/receive/delete works with hardcoded queue name of `myqueue`.
+
+## Demo
+
+You can send messages to WonderQ and see the demo queue stats at our [front end](http://quick-start-dev-serverlessdeploymentbucket-ntikgcvze067.s3-website-us-east-1.amazonaws.com/index.html).
+
+## Setup
+
+1. [Install serverless globally](https://serverless.com/framework/docs/providers/aws/guide/installation/) and add (for example) aws credentials.
+
+2. Clone the repo
+
+3. To use your own Redis server, update REDIS options in handler.js and the VPC info in serveless.yml (TODO: move redis options to environment variables)
+
+4. Run `npm install`
+
+5. Run `sls deploy`. You should get the full endpoints in the output.
+
+6. Client/dist contains index.html. To setup a client bucket, update bucketName in serverless.yml
+
+7. Change `axios.defaults.baseURL` in index.html to your endpoint's baseURL.
+
+8. Run `sls client deploy`
+
+## Endpoints
+
+**POST /init**
+demo: https://rnyin3rr0e.execute-api.us-east-1.amazonaws.com/dev/init
+
+Parameters: body - required
+Ex:
+`{ 'name': 'myqueue', 'vt': 30 }`
+
+Creates a new queue.
+
+**Please note**: current code for below endpoints hardcodes queue name as 'myqueue'. Expect this to be updated in future versions.
+
+**GET /queues**
+demo: https://rnyin3rr0e.execute-api.us-east-1.amazonaws.com/dev/queues
+
+Parameters: none
+
+Lists all queues.
+
+**POST /messages/send**
+demo: https://rnyin3rr0e.execute-api.us-east-1.amazonaws.com/dev/messages/send
+Parameters: body - required
+Ex:
+`{ 'my message' }`
+
+Adds message to queue, returns message ID.
+
+**GET /messages/receive**
+demo: https://rnyin3rr0e.execute-api.us-east-1.amazonaws.com/dev/messages/receive
+
+Parameters: none
+
+Receive top (visible) message from the queue.
+
+**POST /messages/delete**
+demo: https://rnyin3rr0e.execute-api.us-east-1.amazonaws.com/dev/messages/delete
+
+Parameters: body -> messageID - required
+Ex:
+`{ 'f08zycfmxp0M0vQWGweN5hI4mwUxgncc' }`
+
+**GET /info**
+demo: https://rnyin3rr0e.execute-api.us-east-1.amazonaws.com/dev/info
+
+Parameters: none
+
+Get developer info about the message queue.
---
+++ handler.js
@@ -0,0 +1,241 @@
+'use strict';
+const RSMQPromise = require('rsmq-promise');
+import { success, failure } from './libs/response-lib';
+
+const rsmqOptions = {
+  host: 'wonderq.bewcvz.ng.0001.use1.cache.amazonaws.com',
+  port: 6379,
+  ns: 'rsmq'
+};
+
+export async function init(event, context, callback) {
+  //Instruct the lambda to exit immediately
+  //and not wait for node event loop to be empty.
+  //https://stackoverflow.com/questions/43532359/serverless-function-always-timeout-on-lambda/43533732#43533732
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const rsmq = new RSMQPromise(rsmqOptions);
+  const queueName = JSON.stringify(event.body.name);
+  const vt = JSON.stringify(event.body.vt);
+  console.log(event.body);
+
+  function createQueue() {
+    return new Promise(function(resolve, reject) {
+      rsmq
+        .createQueue({
+          qname: queueName,
+          vt: vt
+        })
+        .then(done => {
+          resolve('queue created');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
+  }
+  try {
+    const result = await createQueue();
+    if (result) {
+      callback(null, success(result));
+    } else {
+      callback(null, failure({ status: false, error: 'queue not created.' }));
+    }
+  } catch (e) {
+    //TODO work through lambda error callback
+    console.log(e);
+    callback(null, failure({ status: false }));
+  }
+}
+
+export async function listQueues(event, context, callback) {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const rsmq = new RSMQPromise(rsmqOptions);
+
+  function getQueues() {
+    return new Promise(function(resolve, reject) {
+      rsmq
+        .listQueues()
+        .then(queues => {
+          resolve(queues);
+        })
+        .catch(err => {
+          console.log(err);
+          //TODO work through lambda error callback
+          // callback(err);
+        });
+    });
+  }
+
+  try {
+    const result = await getQueues();
+    if (result) {
+      callback(null, success(result));
+    } else {
+      callback(null, failure({ status: false, error: 'queues not found.' }));
+    }
+  } catch (e) {
+    //TODO work through lambda error callback
+    console.log(e);
+    callback(null, failure({ status: false }));
+  }
+}
+
+export async function sendMessage(event, context, callback) {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const rsmq = new RSMQPromise({ rsmqOptions });
+
+  const sentMessage = JSON.stringify(event.body);
+  function sendMessageToQueue() {
+    return new Promise(function(resolve, reject) {
+      rsmq
+        .sendMessage({
+          qname: 'myqueue',
+          message: sentMessage
+        })
+        .then(result => {
+          console.log('Message sent. ID:', result);
+          resolve(result);
+        })
+        .catch(err => {
+          console.log(err);
+          //TODO work through lambda error callback
+          //callback(err);
+        });
+    });
+  }
+
+  try {
+    const result = await sendMessageToQueue();
+    console.log('message sent, heres the id:', result);
+
+    if (result) {
+      callback(null, success(result));
+    } else {
+      callback(
+        null,
+        failure({ status: false, error: 'message id not returned' })
+      );
+    }
+  } catch (e) {
+    //TODO work through lambda error callback
+    console.log(e);
+    callback(null, failure({ status: false }));
+  }
+}
+
+export async function receiveMessage(event, context, callback) {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const rsmq = new RSMQPromise({ rsmqOptions });
+
+  function receiveMessageFromQueue() {
+    return new Promise(function(resolve, reject) {
+      rsmq
+        .receiveMessage({
+          qname: 'myqueue'
+        })
+        .then(message => {
+          console.log('Received message with ID: ', message.id);
+          resolve(message);
+        })
+        .catch(err => {
+          console.log(err);
+          //TODO work through lambda error callback
+          //callback(err);
+        });
+    });
+  }
+
+  try {
+    const result = await receiveMessageFromQueue();
+
+    if (result) {
+      callback(null, success(result));
+    } else {
+      callback(null, failure({ status: false, error: 'message not received' }));
+    }
+  } catch (e) {
+    //TODO work through lambda error callback
+    console.log(e);
+    callback(null, failure({ status: false }));
+  }
+}
+
+export async function deleteMessage(event, context, callback) {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const rsmq = new RSMQPromise({ rsmqOptions });
+  const idOfMessageToDelete = JSON.parse(event.body);
+  console.log(idOfMessageToDelete);
+  function deleteMessageFromQueue() {
+    return new Promise(function(resolve, reject) {
+      rsmq
+        .deleteMessage({
+          qname: 'myqueue',
+          id: idOfMessageToDelete
+        })
+        .then(result => {
+          console.log('1 - deleted; 2 - not deleted', result);
+          resolve(result);
+        })
+        .catch(err => {
+          console.log(err);
+          //TODO work through lambda error callback
+          //callback(err);
+        });
+    });
+  }
+
+  try {
+    const result = await deleteMessageFromQueue();
+
+    if (result) {
+      callback(null, success(result));
+    } else {
+      callback(null, failure({ status: false, error: 'message not deleted' }));
+    }
+  } catch (e) {
+    //TODO work through lambda error callback
+    console.log(e);
+    callback(null, failure({ status: false }));
+  }
+}
+
+export async function getInfo(event, context, callback) {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const rsmq = new RSMQPromise({ rsmqOptions });
+
+  function getQueueInfo() {
+    return new Promise(function(resolve, reject) {
+      rsmq
+        .getQueueAttributes({
+          qname: 'myqueue'
+        })
+        .then(result => {
+          console.log('Info Received', result);
+          resolve(result);
+        })
+        .catch(err => {
+          console.log(err);
+          callback(err);
+        });
+    });
+  }
+
+  try {
+    const result = await getQueueInfo();
+    console.log('heres the info about myqueue', result);
+
+    if (result) {
+      callback(null, success(result));
+    }
+  } catch (e) {
+    //TODO work through lambda error callback
+    console.log(e);
+    callback(null, failure({ status: false }));
+  }
+}
